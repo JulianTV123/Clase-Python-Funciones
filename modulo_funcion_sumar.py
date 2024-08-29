8e2585142ab3c4563a1ec118f358cf703f7fcf0f
JulianTocasuche <julistv1234@gmail.com>
2024-08-29 09:32:59 -0500

    Initial commit

---
+++ modulo_funcion_sumar.py
@@ -0,0 +1,7 @@
+# Definimos la funcion
+def sumar(a, b):
+    resultado_suma = a + b
+    return resultado_suma
+
+if __name__ == '__main__':
+    print(f'Test: {sumar(15, 30)}')
---
+++ sumar.py
@@ -0,0 +1,9 @@
+from modulo_funcion_sumar import sumar
+
+if __name__ == '__main__':
+    print('*** Función sumar ***')
+    resultado_funcion = sumar(8, 5)
+    print(f'Resultado función sumar: {resultado_funcion}')
+
+#resultado_funcion = sumar(9, 15)
+#print(f'Resultado función sumar: {resultado_funcion}')
